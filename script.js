ee55b78efd5844a6ce0b7e7f036732612d68edbc
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-12-26 07:12:19 -0600

    updated form and JS

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function drop(event){
     let data=event.dataTransfer.getData("text")
     event.target.appendChild(document.getElementById(data))
 }
+
+// Form Validation
+function validateForm(event){
+    // Pulls the element and defaults them to false
+    event.preventDefault();
+    const filled=document.querySelectorAll('input[type="checkbox"]');
+    let checked=false;
+
+    // Will loop through to check if a checkbox is checked
+    filled.forEach(checkbox=> {
+        if(checkbox.checked){
+            checked=true;
+        }
+    });
+    // If none are checked, an alert will pop up
+    if(!checked){
+        alert("Please select a checkbox.")
+        return false
+    }
+    return true
+}
